6ae0683e985589fb5fc173d882f11a95055ee293
pan-dya <pandya.limawan@gmail.com>
2023-05-31 00:15:13 +0700

    update faq
    added media and css design

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,20 @@
 import Home from './Pages/Home.js'
+import Faq from './Pages/Faq.js'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar.js'
+import Footer from './Components/Footer.js'
+// import FloatingNav from './FloatingNav.js'
 
 const App = () => {
   return (
     <Router>
+      <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/faq' element={<Faq />}></Route>
       </Routes>
+      <Footer />
+      {/* <FloatingNav/> */}
     </Router>
   )
 }
--- FrontEnd/src/Pages/Faq.js
+++ FrontEnd/src/Pages/Faq.js
@@ -1,23 +1,36 @@
-import './Design/Faq.css'
+import { useState } from 'react'
+import '../Design/Faq.css'
 
 const Faq = () => {
+  const [selected, setSelected] = useState(null)
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null)
+    }
+    setSelected(i)
+  }
+
   return (
-    <section id='faq'>
-      <div className='wrapper'>
-        <div className='accordion'>
-          {data.map((item, i) => (
-            <div className='item'>
-              <div className='title'>
-                <h2>{item.question}</h2>
+    <div>
+      <div className='padding'></div>
+      <h2 className='page-title'>FAQs</h2>
+        <div className='wrapper'>
+          <article className='accordion'>
+            {data.map((item, i) => (
+              <div className='item'>
+                <div className='title' onClick={() => toggle(i)}>
+                  <h2>{item.question}</h2>
+                  <span>{selected === i ? '-' : '+'}</span>
+                </div>
+                <div className={selected === i ? 'content show' : 'content'} >
+                  {item.answer}
               </div>
-              <div className='content'>
-                {item.answer}
               </div>
-            </div>
-          ))}
+            ))}
+          </article>
         </div>
-      </div>
-    </section>
+    </div>
   )
 }
 
