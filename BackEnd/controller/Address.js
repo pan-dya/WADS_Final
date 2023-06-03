a663414b56b74bd18cd7351ed8c8e14ef34b3143
pan-dya <pandya.limawan@gmail.com>
2023-06-03 20:56:41 +0700

    fix edit address issues

--- BackEnd/controller/Address.js
+++ BackEnd/controller/Address.js
@@ -1,35 +1,74 @@
 import Address from "../models/AddressModel.js";
-import  Users  from "../models/UserModel.js";
+import Users from "../models/UserModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const getAddress= async (req,res) =>{
-    try {
-        const address = await Address.findAll({
-            attributes:['id','province','city', 'regency', 'details', 'postal_code','userId']
-        });
-        res.json(address);
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const getAddress = async (req, res) => {
+  const userId = req.params.userId;
 
-export const add_Address= async(req,res)=>{
-    const {province,city,regency,details,postal_code,userId} = req.body;
-    // const userId = req.params.userId;
-    // if(province === null || city === null || regency === null || details === null || postal_code === null) return res.status(400).json({msg: "There Are Empty Fields, Please Fill All Fields First Before Registering"});
-    try {
-        await Address.create({
-            province: province,
-            city: city,
-            regency: regency,
-            details: details,
-            postal_code: postal_code,
-            userId: userId
-        })
-        res.json({msg:"Address Successfully Added"})
-    } catch (error) {
-        console.log(error);
+  try {
+    const address = await Address.findAll({
+      where: {
+        userId: userId,
+      },
+      attributes: [
+        "id",
+        "province",
+        "city",
+        "regency",
+        "details",
+        "postal_code",
+        "userId",
+      ],
+    });
+    res.json(address);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const add_Address = async (req, res) => {
+  const { province, city, regency, details, postal_code, userId } = req.body;
+  // const userId = req.params.userId;
+  // if(province === null || city === null || regency === null || details === null || postal_code === null) return res.status(400).json({msg: "There Are Empty Fields, Please Fill All Fields First Before Registering"});
+  try {
+    await Address.create({
+      province: province,
+      city: city,
+      regency: regency,
+      details: details,
+      postal_code: postal_code,
+      userId: userId,
+    });
+    res.json({ msg: "Address Successfully Added" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateAddress = async (req, res) => {
+  const { province, city, regency, details, postal_code } = req.body;
+  const { userId } = req.params;
+
+  try {
+    const address = await Address.findOne({
+      where: { userId: userId },
+    });
+    if (!address) {
+      return res.status(404).json({ msg: "Address not found" });
     }
-    
-}
+
+    address.province = province;
+    address.city = city;
+    address.regency = regency;
+    address.details = details;
+    address.postal_code = postal_code;
+
+    await address.save();
+
+    res.json({ msg: "Address successfully updated" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
+};
--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getUsers,Register, Login, Logout } from "../controller/Users.js";
-import { getAddress, add_Address } from "../controller/Address.js";
+import { getAddress, add_Address, updateAddress } from "../controller/Address.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { refreshToken } from "../controller/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/address',getAddress);
+router.get('/address/:userId',getAddress);
 router.post('/address', add_Address);
+router.put('/address/:userId', updateAddress);
 router.get('/users',verifyToken,getUsers);
 router.post('/users',Register);
 router.post('/login',Login);
